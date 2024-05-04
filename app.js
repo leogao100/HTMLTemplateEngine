27d6e82a02e8d761059cb7d3219de605471cc116
xixismile <gaoleiccie@gmail.com>
2024-05-04 14:15:57 +1000

    users add page in three different template engines

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser"); 
+const { engine } = require("express-handlebars")
+
+const app = express();
+
+const users = [];
+
+// app.engine("hbs", engine({ defaultLayout: "main-layout", extname:"hbs"}));
+
+app.set("view engine", "ejs");
+// app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get("/", (req, res, next) => {
+  res.render("index", { pageTitle: "Add User" });
+});
+
+app.get("/users", (req, res, next) => {
+  res.render("users", { pageTitle: "User", users: users, hasUsers: users.length > 0 });
+});
+
+app.post("/add-user", (req, res, next) => {
+  users.push({name: req.body.username});
+  res.redirect("/users");
+});
+
+app.listen(3000);
